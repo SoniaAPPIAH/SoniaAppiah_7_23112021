8121962457c635e49ae23f798bd179305c822ab9
Sonia APH <sonia.appiah1@gmail.com>
2021-12-29 23:07:32 +0100

    Header Home React

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userCtrl = require('../controllers/userCtrl');
-
-router.post('/signup', userCtrl.signup);
-
-module.exports = router;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+    return (
+        <header className="Header">
+            <img className="logo" src="./img/logo/icon-left-font.svg" alt="Groupomania" />
+        </header>
+    )
+}
+
+export default Header
