593ec2130151d4c0fbf95bead3983344d012099c
Sonia APH <sonia.appiah1@gmail.com>
2021-12-29 14:08:59 +0100

    Connection DB

--- backend/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const userRoutes = require('./routes/user');
-const app = express();
-
-app.use('api/user', userRoutes);
-
-
-module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,47 +1,20 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'sampleDB',
 });
 
-server.listen(port);
+connection.connect(function(error) {
+    if(!error) {
+        console.log('Error');
+    } else {
+        console.log('Connected');
+    }
+});
+
+app.listen
