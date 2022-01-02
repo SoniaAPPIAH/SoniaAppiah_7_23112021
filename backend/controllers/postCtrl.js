c948a00c0e0e16bf432780914dfa57c179764063
Sonia APH <sonia.appiah1@gmail.com>
2022-01-02 12:40:29 +0100

    Post Ctrl getAll

--- backend/controllers/postCtrl.js
+++ backend/controllers/postCtrl.js
@@ -4,6 +4,7 @@ const fs = require("fs");
 exports.createPost = async (req, res) => {
     const image = (req.file) ? `${req.protocol}://${req.get('host')}/images/post/${req.file.filename}` : "";
     const messagePost = (req.body.message) ? req.body.message : " ";
+    
     const post = {
         ...req.body,
         message: messagePost,
@@ -11,16 +12,18 @@ exports.createPost = async (req, res) => {
         like: 0,
         datetime: new Date().toLocaleString("af-ZA", { timeZone: "Europe/Paris" }),
     };
-    //ENVOIE LA REQUETE AVEC MULTER ET LES VALEURS PAR DEFAUT
+
     db.query("INSERT INTO post SET ?", post, function (err, result) {
         if (err) throw err;
         res.status(201).json({ message: `Post ajouté` });
     })
 };
 
-
-exports.readPost = async (req, res) => {
-
+exports.getAllPosts = async (req, res) => {
+    db.query("SELECT * FROM post p JOIN Users WHERE id=authorId ORDER BY datetime DESC LIMIT 50;", function (err, result) {
+        if (err) res.status(400).json({ err });
+        res.status(200).json(result)
+    });
 };
 
 exports.updatePost = async (req, res) => {
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const postCtrl = require('../controllers/postCtrl');
 
-router.get('/', postCtrl.readPost);
+router.get('/', postCtrl.getAllPosts);
 router.post('/', postCtrl.createPost);
 router.put('/:d', postCtrl.updatePost);
 router.delete('/:d', postCtrl.deletePost);
